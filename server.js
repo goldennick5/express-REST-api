91b6b1acafbc58bb2052c0f27a8e86be988cda9d
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-05-25 22:48:26 +0600

    fix

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const connectDB = require("./data/db");
 const router = require("./routes/index");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use('/', router);
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", router);
 
 try {
   app.listen(port, () => {
